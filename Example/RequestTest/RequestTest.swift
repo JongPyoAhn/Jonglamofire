c1c40a85b63aa140c68dabe4cee9220815ae77ba
JongPyoAhn <whdvy95@gmail.com>
2023-03-16 12:56:31 +0900

    test unit 생성

---
+++ Example/RequestTest/RequestTest.swift
@@ -0,0 +1,18 @@
+//
+//  RequestTest.swift
+//  RequestTest
+//
+//  Created by 안종표 on 2023/03/16.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+final class RequestTest: XCTestCase {
+    func testRequestSuccess(){
+        
+    }
+    func testRequestFailure(){
+        
+    }
+}
